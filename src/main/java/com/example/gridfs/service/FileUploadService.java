eadd57845ffa16ca440937fdbc1a283ac45c33a1
Erdem Şahlan <sahlanerdem@gmail.com>
2021-04-26 21:10:17 +0300

    first commit

---
+++ src/main/java/com/example/gridfs/service/FileUploadService.java
@@ -0,0 +1,25 @@
+package com.example.gridfs.service;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileUploadService {
+
+    @Autowired
+    private GridFsTemplate gridFsTemplate;
+
+    public void uploadFile(MultipartFile file) throws Exception
+    {
+        DBObject dbObject=new BasicDBObject();
+        dbObject.put("fileName",file.getName());
+        dbObject.put("contentType",file.getContentType());
+        dbObject.put("size",file.getSize());
+        dbObject.put("userId","12345");
+        gridFsTemplate.store(file.getInputStream(),file.getContentType(),dbObject);
+    }
+}
